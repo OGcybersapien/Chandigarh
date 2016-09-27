0f5cdefb6c4766dba459a87e16fa920eacdba9af
OGCybersapien <aditya@cybersapien.xyz>
2016-09-27 18:31:21 +0530

    Added dpi-scaled drawables, need to see how nicely they work on different sized devices

--- app/src/main/java/xyz/cybersapien/chandigarh/PlacesOfWorship.java
+++ app/src/main/java/xyz/cybersapien/chandigarh/PlacesOfWorship.java
@@ -19,11 +19,11 @@ public class PlacesOfWorship extends AppCompatActivity {
 
         //Creating an ArrayList of Object Place with information like Place Name, Address and the Image Drawable Id
         final ArrayList<Place> places = new ArrayList<>();
-        places.add(new Place("Mata Mansa Devi Temple", "Mansa Devi Complex\nSector 4\nPanchkula",R.drawable.rock_garden));
-        places.add(new Place("Saketri Shiva Temple", "Saketri\nPanchkula",R.drawable.rock_garden));
-        places.add(new Place("Nada Sahib Gurudwara", "Nada Sahib\nPanchkula",R.drawable.rock_garden));
-        places.add(new Place("Pracheen Chandi Mandir", "Bhagwanpur\nPanchkula",R.drawable.rock_garden));
-        places.add(new Place("Sai Baba Temple", "Sector 29\nChandigarh",R.drawable.rock_garden));
+        places.add(new Place("Mata Mansa Devi Temple", "Mansa Devi Complex\nSector 4\nPanchkula",R.drawable.mansa_devi));
+        places.add(new Place("Saketri Shiva Temple", "Saketri\nPanchkula",R.drawable.saketri));
+        places.add(new Place("Nada Sahib Gurudwara", "Nada Sahib\nPanchkula",R.drawable.nada_sahib));
+        places.add(new Place("Pracheen Chandi Mandir", "Bhagwanpur\nPanchkula",R.drawable.chandi_mandir));
+        places.add(new Place("Sai Baba Temple", "Sector 29\nChandigarh",R.drawable.sai));
 
         //Creating a custom Array Adapter for this Tourist Attractions
         PlacesAdapter placesAdapter = new PlacesAdapter(this, places);
